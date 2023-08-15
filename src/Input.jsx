ebd93e6dc6219e6e497279c89b8c09607d033529
Kevin Ireri <117742907+Mureria@users.noreply.github.com>
2023-08-15 23:47:51 +0300

    2nd update

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
 const Input = () => {
+        const [value, setValue] = useState("");
+    
+        const handleSubmit = e => {
+            e.preventDefault();
+            if (!value) return;
+            
+            addTask(value);
+            setValue("");
+        }
+    
+
   return (
     <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='flex items-center gap-4 my-4'>
                 <div className='basis-[90%]'>
-                    <input type="text" name="" id="" placeholder='Add Todo' className='w-full py-2 px-4 text-sm md:text-base' />
+                    <input value={value} type="text" name="" id="" placeholder='Add Todo'
+                     onChange={e => setValue(e.target.value)} className='w-full py-2 px-4 text-sm md:text-base'
+                    />
                 </div>
                 <div className='basis-[20%]'>
                     <button className='w-full border px-4 py-2 text-sm md:text-base'>Add</button>
                 </div>
+                
             </div>
         </form>
         
